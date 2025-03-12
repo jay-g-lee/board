505f152afee8a9adad9e33c318a41bb757216052
jay-g-lee <jay-g@cntt.co.kr>
2025-03-12 15:29:58 +0900

    Initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+//import variables from './../variables.module.scss'
+
+export default function Dashboard() {
+    return (
+      <div>
+        <p>This is the Dashboard page.</p>
+      </div>
+    );
+}
